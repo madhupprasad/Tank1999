e4e9c0874f4f689c3e18943f53438b77d5debf35
Madhupprasad <madhupprasad7@gmail.com>
2024-07-31 21:52:53 +0530

    change;

--- bullet.gd
+++ bullet.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
-const SPEED = 800.0
+const SPEED = 1300.0
 var area_direction = Vector2(0, 0)
 var debounce = false
 
 func _process(delta):
-	self.translate(area_direction * SPEED * delta)
+	self.position += area_direction * SPEED * delta
 
 func _on_body_entered(body):
 	# Stops an error that crashes the game.
